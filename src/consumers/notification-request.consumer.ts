4d80fabb4d16d3f30925d7e6321083edc3eb83b7
Alin Florin <alin@qoffice.ro>
2024-05-19 00:05:19 +0300

    -

--- src/consumers/notification-request.consumer.ts
+++ src/consumers/notification-request.consumer.ts
@@ -4,6 +4,7 @@ import { NotificationRequest } from "../contracts/notification-request";
 export const notificationRequestConsumer = async (message: ConsumeContext<NotificationRequest>) => {
   console.log(message.message);
   console.log(message.headers);
+  console.log(message.sentTime);
 }
 
 export default notificationRequestConsumer;
