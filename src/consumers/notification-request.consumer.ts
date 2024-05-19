25a11a291fddf91c684e278a472b0e9a7c2e828e
Alin Florin <alin@qoffice.ro>
2024-05-19 03:54:54 +0300

    -

--- src/consumers/notification-request.consumer.ts
+++ src/consumers/notification-request.consumer.ts
@@ -3,11 +3,15 @@ import bus from "../services/masstransit.service";
 import { MessageType } from "masstransit-rabbitmq/dist/messageType";
 
 export const startNotificationRequestConsumer = () => {
-  bus.asyncReceiveEndpoint('huna-notifications', endpoint => {
-    endpoint.handle<NotificationRequest>(new MessageType('NotificationRequest', 'Huna.Notifications.Contracts'),
-      async context => {
-        console.log(context.message);
-      });
+  bus.asyncReceiveEndpoint("huna-notifications", (endpoint) => {
+    endpoint.handle<NotificationRequest>(
+      new MessageType("NotificationRequest", "Huna.Notifications.Contracts"),
+      async (context) => {
+        console.log(context.messageId);
+      }
+    );
+  }, {
+    maxRetries: 5,
   });
-}
+};
 export default startNotificationRequestConsumer;
